5d21877d5edaa6e5f82df424fcc18ec0976af1b4
VBogatyreva <156297020+VBogatyreva@users.noreply.github.com>
2026-03-13 11:18:50 +0300

    Added interface ScheduleRepository

---
+++ app/src/main/java/ru/bogatyreva/class_schedule/domain/ScheduleRepository.kt
@@ -0,0 +1,19 @@
+package ru.bogatyreva.class_schedule.domain
+
+import kotlinx.coroutines.flow.Flow
+import java.time.Instant
+
+interface ScheduleRepository {
+
+    fun getLessonsForDate(date: Instant): Flow<List<Lesson>>          // Основной метод - получение списка уроков
+    suspend fun getFormattedDate(date: Instant): String               // Для шапки - "25 марта, вторник"
+    suspend fun getSemesterDisplayName(): String                      // Для шапки - "Осенний семестр 2025"
+    suspend fun getLessonsCountForDate(date: Instant): Int            // Для шапки - количество пар
+    suspend fun getCalendarDates(centerDate: Instant): List<Instant>  // Для календаря - какие даты показывать
+    suspend fun getDayNumber(date: Instant): String                   // Для календаря - числа (24, 25, 26)
+    suspend fun getDayOfWeek(date: Instant): String                   // Для календаря - дни недели (Пн, Вт)
+    suspend fun getToday(): Instant                                   // Для навигации - кнопка "Сегодня"
+    suspend fun isToday(date: Instant): Boolean                        // Для календаря - подсветка сегодня
+    suspend fun isSelected(date: Instant, selectedDate: Instant): Boolean   // Для календаря - подсветка выбранного
+
+}
